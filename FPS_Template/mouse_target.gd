3baecd48af233250ebe1b00ee11bdd578121eb8b
KyleNorlander <54556328+KyleNorlander@users.noreply.github.com>
2019-11-25 23:14:12 -0500

    adding sound

---
+++ FPS_Template/mouse_target.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	
+func _physics_process(delta):
+	position = get_global_mouse_position()
+	
+	if Input.is_action_just_pressed("Left_Mouse"):
+		$Sound.play()
